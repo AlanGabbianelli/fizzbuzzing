2d8dc8b56b456bfd646e63b7bc15509a5d90f067
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2016-01-15 18:03:35 +0000

    Method refactoring

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,9 @@
-def fizzbuzz number
-
-  if number % 3 == 0 && number % 5 == 0
-    "fizzbuzz"
+def fizzbuzz(number)
+  if number % 15 == 0
+    'fizzbuzz'
   elsif number % 3 == 0
-    "fizz"
-  elsif number % 5 ==0
-    "buzz"
+    'fizz'
+  elsif number % 5 == 0
+    'buzz'
   end
-
 end
