f72874866f47c0b7e6c6620fffb238c4948212cf
arnoldmanzano <arnold.manzano@gmail.com>
2016-01-15 18:08:16 +0000

    Added and passed test for non-divisible number

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -16,4 +16,9 @@ describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+
+  it 'returns given number when non-divisible by 3 or 5' do
+    expect(fizzbuzz(4)).to eq 4
+  end
+  
 end
