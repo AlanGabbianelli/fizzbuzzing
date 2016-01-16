cb239ff218cbe696237596e6067b505767d322e0
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2016-01-16 19:54:45 +0000

    Final refactoring

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,24 +1,43 @@
 require 'fizzbuzz'
 
-describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+describe 'FizzBuzz' do
+  context 'numbers divisible by 3' do
+    it 'returns "Fizz" when passed 3' do
+      expect(fizzbuzz(3)).to eq 'Fizz'
+    end
+
+    it 'returns "Fizz" when passed 48' do
+      expect(fizzbuzz(48)).to eq 'Fizz'
+    end
   end
 
-  it 'returns "fizz" when passed 6' do
-    expect(fizzbuzz(6)).to eq 'fizz'
+  context 'numbers divisible by 5' do
+    it 'returns "Buzz" when passed 5' do
+      expect(fizzbuzz(5)).to eq 'Buzz'
+    end
+
+    it 'returns "Buzz" when passed 70' do
+      expect(fizzbuzz(70)).to eq 'Buzz'
+    end
   end
 
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+  context 'numbers divisible by 3 and 5' do
+    it 'returns "FizzBuzz" when passed 15' do
+      expect(fizzbuzz(15)).to eq 'FizzBuzz'
+    end
+
+    it 'returns "FizzBuzz" when passed 75' do
+      expect(fizzbuzz(75)).to eq 'FizzBuzz'
+    end
   end
 
-  it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
-  end
+  context 'numbers non-divisible by 3 or 5' do
+    it 'returns given number when passed 4' do
+      expect(fizzbuzz(4)).to eq 4
+    end
 
-  it 'returns given number when non-divisible by 3 or 5' do
-    expect(fizzbuzz(4)).to eq 4
+    it 'returns given number when passed 7' do
+      expect(fizzbuzz(7)).to eq 7
+    end
   end
-  
 end
